d8c76315434d488e69de8c8b8d169f45c1dc990e
himanshu <himanshusharmahs974@gmail.com>
2016-10-23 18:44:10 +0530

    Added Toast button

--- app/src/main/java/com/himanshusharmahs974gmail/vibgyorlinear/MainActivity.java
+++ app/src/main/java/com/himanshusharmahs974gmail/vibgyorlinear/MainActivity.java
@@ -2,6 +2,7 @@ package com.himanshusharmahs974gmail.vibgyorlinear;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+Button button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=(Button)findViewById(R.id.bu);
+        button.setOnClickListner(new OnClickListner(){
+            public void onClick(View v){
+                Toast.makeText(getBaseContext(), "Vibgyor app!" , Toast.LENGTH_SHORT ).show()}});
+        
+        
     }
 }
